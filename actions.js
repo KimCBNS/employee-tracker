d30e66ec18352c1fc718e0f4ec4bf57342631eb9
KimCBNS <desveaux.kim@gmail.com>
2024-07-21 17:06:29 -0300

    deeper understanding of async await functions and first switch response is working

--- actions.js
+++ actions.js
@@ -8,23 +8,81 @@
 //   '7. Update an employee role'
 
 
+
+
+
+// use this later to end the session
+// function closeConnection() {
+//   if (client) {
+//     client.end();
+//   }
+// }
+
+/////////
+
+
+// set up the SQL connections
+const { Pool } = require('pg');
+
+const pool = new Pool(
+  {
+    // TODO: Enter PostgreSQL username
+    user: 'postgres',
+    // TODO: Enter PostgreSQL password
+    password: 'password',
+    host: 'localhost',
+    database: 'company_db'
+  },
+  console.log(`Connected to the company_db database.`)
+)
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connected to the company_db database.');
+  release();
+});
+
+async function viewDepartments() {
+  try {
+    const res = await pool.query('SELECT id, department_name AS department FROM department');
+    return res.rows;
+  } catch (err) {
+    console.error('Error viewing departments:', err.message);
+    throw err;
+  }
+}
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function doSQLTask(data) {
-
+async function doSQLTask(data) {
+  // set up the SQL connections
   switch (data.ToDo) {
     case '1. View all departments':
       console.log("1");
+      // need to do a SQL query to get all the data
+      // Await the result of the viewDepartments function
+      try {
+        const departmentReturn = await viewDepartments();
+        console.table(departmentReturn);
+      } catch (err) {
+        console.error('Error retrieving departments:', err.message);
+      }
       break;
+
     case '2. View all roles':
       console.log("2");
       break;
+
     case '3. View all employees':
       console.log("3");
       break;
+
     case '4. Add a department':
       console.log("4");
       break;
+
     case '5. Add a role':
       console.log("5");
       break;
@@ -45,6 +103,7 @@ function doSQLTask(data) {
   console.log(`data is here ${data.ToDo} now `);
 
 };
+
 
 
 module.exports = doSQLTask;
